022e3bc223c9468024193f5e71612e7d2243248f
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-07 08:00:49 +0100

    Correcting Hover Graph Bug
    - Correcting Hover Graphs Bug
    - Draw Chart once when the page is loaded
    - Refactoring Functions

--- app/assets/javascripts/statistics_expenses_breaks.js
+++ app/assets/javascripts/statistics_expenses_breaks.js
@@ -4,55 +4,134 @@
 var ready;
 ready = function() {
 
-  /*
-  *
-  *
-  *
-  *
-  * Filter Breaks By Year
-  *
-  *
-  *
-  *
-  */
+  //
+  //
+  // Add Message for No Data
 
   //
   //
-  // Draw Chart
-  function drawChartByYear(breaks_months, breaks_data){
+  // Global Variables
+  var canvas_ids = ['BreaksExpensesStats', 'BreaksExpensesStats_2'];
+  var charts = [];
+  var current_year = moment().year();
+  var current_month = moment().month() + 1;
 
-    var breaks_stats_id = 'BreaksExpensesStats';
-    var breaks_stats = document.getElementById(breaks_stats_id);
+  //
+  //
+  // Functions
+  function drawChart(chart_ids){
 
-    if (breaks_stats){
-      var breaks = breaks_stats.getContext('2d');
+    var options = {
+      elements: {
+        line: {
+          tension: 0.000001
+        }
+      },
+      plugins: {
+        filler: {
+          propagate: true
+        }
+      }
+    };
 
-      //
-      // Draw Chart
-      new Chart(breaks, {
-        type: 'bar',
-        data: {
-          labels: breaks_months,
-          datasets: [{
-            data: breaks_data,
-            label: "Arrêts",
-            borderColor: "rgba(184, 97, 22, 0.7)",
-            backgroundColor: "rgba(184, 97, 22, 0.3)",
-            borderWidth: 2,
-            fill: 'origin'
-          }]
-        },
-        options: {
-          elements: {
-            line: {
-              tension: 0.000001
+    for (var chart_id in chart_ids) {
+
+        var breaks_stats = document.getElementById(chart_ids[chart_id]);
+
+        if (breaks_stats) {
+          var breaks = breaks_stats.getContext('2d');
+
+          charts[chart_id] = new Chart(breaks, {
+            type: 'bar',
+            options: options
+          });
+
+        }
+      }
+
+  }
+  function updateChart(option, labels, data){
+
+    if (charts[0] && option === 'by_year'){
+      charts[0].reset();
+      charts[0].data = {
+        labels: labels,
+        datasets: [{
+          data: data,
+          label: "Arrêts",
+          borderColor: "rgba(184, 97, 22, 0.7)",
+          backgroundColor: "rgba(184, 97, 22, 0.3)",
+          borderWidth: 2,
+          fill: 'origin'
+        }]
+      };
+      charts[0].update();
+    }
+
+    if (charts[1] && option === 'by_year_and_month'){
+      console.log(labels)
+      charts[1].data = {
+        labels: labels,
+        datasets: [{
+          data: data,
+          label: "Arrêts",
+          borderColor: "rgba(184, 97, 22, 0.7)",
+          backgroundColor: "rgba(184, 97, 22, 0.3)",
+          borderWidth: 2,
+          fill: 'origin'
+        }]
+      };
+      charts[1].update();
+    }
+
+  }
+  function drawChartAjax(option, selected_year, selected_month){
+
+    if (option === 'by_year'){
+      console.log('by_year');
+      $.ajax({
+        url: '/statistics/get_breaks_by_year',
+        type: 'GET',
+        dataType: 'json',
+        data: { break_year: selected_year },
+        success: function(data){
+
+          var chart_months = [];
+          var chart_data = [];
+
+          if (data) {
+            for (var months in data){
+              chart_months.push(months)
+              chart_data.push(data[months])
             }
-          },
-          plugins: {
-            filler: {
-              propagate: true
-            }
+            updateChart('by_year', chart_months, chart_data)
           }
+
+        }
+      });
+    }
+    else if (option === 'by_year_and_month') {
+      console.log('by_year_and_month');
+      $.ajax({
+        url: '/statistics/get_breaks_by_year_and_month',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          break_year: selected_year,
+          break_month: selected_month
+        },
+        success: function(data){
+          var chart_days = [];
+          var chart_data = [];
+
+          if (data) {
+            for (var days in data){
+              chart_days.push(days);
+              chart_data.push(data[days]);
+            }
+            updateChart('by_year_and_month', chart_days, chart_data);
+          }
+
         }
       });
     }
@@ -61,156 +140,37 @@ ready = function() {
 
   //
   //
-  // Getting Data with Ajax and Draw the Chart
-  function drawChartAjaxByYear(selected_year){
-    $.ajax({
-      url: '/statistics/get_breaks_by_year',
-      type: 'GET',
-      dataType: 'json',
-      data: { break_year: selected_year },
-      success: function(data){
-
-        var chart_months = [];
-        var chart_data = [];
-
-        if (data) {
-          for (var months in data){
-            chart_months.push(months)
-            chart_data.push(data[months])
-          }
-          drawChartByYear(chart_months, chart_data);
-        }
-
-      }
-    });
-  }
+  // Draw charts
+  drawChart(canvas_ids);
 
   //
   //
-  // Draw a Chart for the Current Year
-  var current_year = moment().year();
-  drawChartAjaxByYear(current_year);
+  // Draw Charts With the Current Date
+  drawChartAjax('by_year', current_year);
+  drawChartAjax('by_year_and_month', current_year, current_month);
 
   //
   //
-  // Get Breaks By Year
+  // Clicks
   $('#get_breaks_by_year').on('click', function(){
 
     var selected_year = $(this).parent().find('input[type="text"]').val();
 
     if (selected_year !== ''){
-
-      drawChartAjaxByYear(selected_year);
-
+      drawChartAjax('by_year', selected_year);
     }
 
     return false;
 
   });
-
-
-  /*
-  *
-  *
-  *
-  *
-  * Filter Breaks By Year and Month
-  *
-  *
-  *
-  *
-  */
-
-  //
-  //
-  // Draw Chart
-  function drawChartByYearAndMonth(breaks_start_and_end_date, breaks_data){
-
-    var breaks_stats_id = 'BreaksExpensesStats_2';
-    var breaks_stats = document.getElementById(breaks_stats_id);
-
-    if (breaks_stats){
-
-      var breaks = breaks_stats.getContext('2d');
-
-      //
-      // Draw Chart
-      new Chart(breaks, {
-        type: 'bar',
-        data: {
-          labels: breaks_start_and_end_date,
-          datasets: [{
-            data: breaks_data,
-            label: "Arrêts",
-            borderColor: "rgba(184, 97, 22, 0.7)",
-            backgroundColor: "rgba(184, 97, 22, 0.3)",
-            borderWidth: 2,
-            fill: 'origin'
-          }]
-        },
-        options: {
-          elements: {
-            line: {
-              tension: 0.000001
-            }
-          },
-          plugins: {
-            filler: {
-              propagate: true
-            }
-          }
-        }
-      });
-    }
-
-  }
-
-  //
-  //
-  // Getting Data with Ajax and Draw the Chart
-  function drawChartAjaxByYearAndMonth(selected_year, selected_month){
-    $.ajax({
-      url: '/statistics/get_breaks_by_year_and_month',
-      type: 'GET',
-      dataType: 'json',
-      data: {
-        break_year: selected_year,
-        break_month: selected_month
-      },
-      success: function(data){
-        var chart_days = [];
-        var chart_data = [];
-
-        if (data) {
-          for (var days in data){
-            chart_days.push(days)
-            chart_data.push(data[days])
-          }
-          drawChartByYearAndMonth(chart_days, chart_data);
-        }
-
-      }
-    });
-  }
-
-  //
-  //
-  // Draw a Chart for the Current Year
-  var current_year = moment().year();
-  var current_month = moment().month() + 1;
-  drawChartAjaxByYearAndMonth(current_year, current_month);
-
-  //
-  //
-  // Get Breaks By Year and Month
   $('#get_breaks_by_year_and_month').on('click', function(){
 
     var selected_date = $(this).parent().find('input[type="text"]').val().split("/");
     var month = selected_date[0];
     var year = selected_date[1];
 
-    if (selected_date !== ''){
-      drawChartAjaxByYearAndMonth(year, month);
+    if (selected_date !== '' && month){
+      drawChartAjax('by_year_and_month', year, month);
     }
 
     return false;
