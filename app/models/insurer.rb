d03312151fc180ee19f7659a800ec91127b3f54d
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-13 22:40:19 +0100

    Add Picture Upload to Insurer

--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -1,2 +1,15 @@
 class Insurer < ApplicationRecord
+
+  validates :name, presence: true
+  validate  :picture_size
+
+  mount_uploader :picture, AttachedFileUploader
+
+  # Validates the size of an uploader picture
+  def picture_size
+    if picture.size > 50.kilobytes
+      errors.add(:picture, 'should be less than 200 KB')
+    end
+  end
+
 end
